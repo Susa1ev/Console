acd0cec4f3d047c04fc588b811604885dd7a7c52
Susa1ev <susalytt@gmail.com>
2021-12-10 21:22:25 +0300

    AddZad7Console

---
+++ Z07C/Z072C/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System;
+
+namespace Z072C
+{
+    class prac
+    {
+        StringBuilder sb = new StringBuilder();
+        string[] words;
+        int[] lengthOfWords;
+        int buf = Int32.MaxValue;
+        public prac()
+        {
+            Main();
+        }
+        void Main()
+        {
+            Console.Write("Введите строку: ");
+            sb.Append(Console.ReadLine());
+            words = sb.ToString().Split();
+            lengthOfWords = new int[words.Length];
+            for(int i = 0; i < words.Length;i++)
+            {
+                lengthOfWords[i] = words[i].Length;
+                if (lengthOfWords[i] < buf) { buf = lengthOfWords[i]; }
+            }
+            for(int i = 0; i < words.Length; i++)
+            {
+                if(words[i].Length == buf)
+                {
+                    Console.Write(words[i]+ " ");
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            prac p = new prac();
+        }
+    }
+}
---
+++ Z07C/Z07C/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+using System;
+
+namespace Z071C
+{
+    class prac
+    {
+        StringBuilder sb = new StringBuilder();
+        char ch;
+        bool flag = false;
+        int bufI;
+        public prac()
+        {
+            Main();
+        }
+        void Main()
+        {
+            Console.Write("Введите строку: ");
+            sb.Append(Console.ReadLine());
+            while(true)
+            {
+                Console.Write("Найти первое и последнее вхождение символа: ");
+                if (char.TryParse(Console.ReadLine(), out ch))
+                {
+                    for(int i = 0; i < sb.Length; i++)
+                    {
+                        
+                        if(sb[i] == ch)
+                        {
+                            bufI = i + 1;
+                            if(!flag)
+                            {
+                                Console.WriteLine($"Символ первый раз встретился в {i + 1} символе в строке");
+                                flag = true;
+                            }
+                        }
+                    }
+                    Console.WriteLine($"Символ последний раз встретился в {bufI} символе в строке");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Вы ввели не символ");
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            prac p = new prac();    
+        }
+    }
+}
