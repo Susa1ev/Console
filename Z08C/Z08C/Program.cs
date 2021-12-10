a445cbead20f3a85f715e422cd9e90a43a0330e7
Susa1ev <susalytt@gmail.com>
2021-12-10 23:16:33 +0300

    AddZ08Console

---
+++ Z08C/Z08C/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+namespace Z08C
+{
+    class prac
+    {
+        public prac()
+        {
+            Main();
+        }
+        StringBuilder sb = new StringBuilder();
+        string[] words;
+        void Main()
+        {
+            Regex r = new Regex(@"(([01]?\d\d?|2[0-4]\d|25[0-5])\.){3}([01]?\d\d?|2[0-4]\d|25[0-5])");
+            Console.Write("Введите строку: ");
+            sb.Append(Console.ReadLine());
+            words = sb.ToString().Split(' ');
+            Console.Write("Все IPv4 в данном тексте: ");
+            for (int i = 0; i < words.Length; i++)
+            {
+                if(r.IsMatch(words[i]))
+                {
+                    Console.Write(words[i] + "    ");
+                }
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            prac p = new prac();
+        }
+    }
+}
