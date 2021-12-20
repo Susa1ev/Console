ba8e8b9641ad4136fbda6ebbd0b2ed99b6120cad
Андрей <95218345+Susa1ev@users.noreply.github.com>
2021-12-20 21:29:04 +0300

    Add files via upload

---
+++ Z06C/Z062/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace prac62console
+{
+    class prac
+    {
+        Random r = new Random();
+        public prac()
+        {
+            Main();
+        }
+        int n;
+        double F = Int32.MinValue;
+        double[] arr;
+        void Main()
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите размер массива:");
+                if (Int32.TryParse(Console.ReadLine(), out n))
+                {
+                    arr = new double[n];
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        arr[i] = r.Next(-10, 9) + r.NextDouble();
+                        Console.WriteLine($"{i + 1}-ый элемент массива: {arr[i]}");
+                        if (arr[i] < 0 && arr[i] > F) { F = arr[i]; }
+                    }
+                    Console.WriteLine("Максимальное число из отрицательных: " + F);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Размер массива должен быть целочисленным");
+                }
+            }            
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            prac p = new prac();
+        }
+    }
+}
+
---
+++ Z06C/prac6.1console/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace prac61console
+{
+    class prac
+    {
+        Random r = new Random();
+        public prac()
+        {
+            InOne();
+            InTwo();
+        }
+        int[] arr1;
+        int[,] arr2;
+        int k, n;
+
+        void InOne()
+        {
+            Console.WriteLine("одномерный:");
+            while (true)
+            {
+                Console.WriteLine("Введите размер массива:");
+                if (Int32.TryParse(Console.ReadLine(), out k))
+                {
+                    arr1 = new int[k];
+                    for (int i = 0; i < arr1.Length; i++)
+                    {
+                        arr1[i] = r.Next(-10,10);
+                        Console.WriteLine($"{i + 1}-ый элемент массива: {arr1[i]}");
+                    }
+                    Console.WriteLine("Нечётные элементы массива:");
+                    for (int i = 0; i < arr1.Length; i++)
+                    {
+                        if (arr1[i] % 2 == 1 || arr1[i] % 2 == -1)
+                        {
+                            Console.Write(arr1[i] + " ");
+                        }
+                    }
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Вы ввели что-то не то");
+                }
+            }
+
+        }
+        void InTwo()
+        {
+            Console.WriteLine("\n\nдвумерный:");
+            Console.WriteLine("Введите размер массива NxN:");
+            while (true)
+            {
+                Console.Write("N: ");
+                if (Int32.TryParse(Console.ReadLine(), out n))
+                {
+                    arr2 = new int[n, n];
+                    for (int i = 0; i < n; i++)
+                    {
+                        for (int j = 0; j < n; j++)
+                        {
+                            arr2[i, j] = r.Next(-10,10);
+                            Console.WriteLine($"[{i+1},{j+1}]-ый элемент матрицы равен - {arr2[i,j]}");
+                        }
+                    }
+                    Console.WriteLine("Нечётные элементы массива:");
+                    for (int i = 0; i < n; i++)
+                    {
+                        for (int j = 0; j < n; j++)
+                        {
+                            if (arr2[i, j] % 2 == 1 || arr2[i, j] % 2 == -1) { Console.Write(arr2[i, j] + " "); }
+                        }
+                    }
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Рамер массива должны быть целлочисленными");
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            prac p = new prac();    
+        }
+    }
+}
