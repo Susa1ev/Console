e1902976bdb6ac6e8b6e04190c79b173a0b4408c
Андрей <95218345+Susa1ev@users.noreply.github.com>
2021-12-20 21:17:22 +0300

    Add files via upload

---
+++ Z02C/Z2.1Console/Z2.1Console/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Z2._1Console
+{
+    class Threeug
+    {
+        double x = 0;
+        double y = 0;
+        public Threeug()
+        {
+            input();
+        }
+        void input()
+        {
+            Try:
+            Console.Write("Введи x ");
+            if (Double.TryParse(Console.ReadLine(), out x))
+            {
+                Console.Write("Введи y ");
+                if (Double.TryParse(Console.ReadLine(), out y))
+                {
+                    sravnenie();
+                }
+                else
+                {
+                    Console.WriteLine("Вы ввели что-то не то");
+                    goto Try;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели что-то не то");
+                goto Try;
+            }
+
+        }
+        public void sravnenie()
+        {
+            if (x < 0 && x > -23 && y < 0 && y > x)
+            {
+                Console.Write("\tНаходится внутри функций\n");
+                input();
+            }
+            else if ((x <= 0 && x >= -23) && y == 0)
+            {
+                Console.Write("\tНаходится на функциях\n");
+                input();
+            }
+            else
+            {
+                Console.Write("\tНаходится вне функций\n");
+                input();
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Threeug p = new Threeug();
+        }
+    }
+}
