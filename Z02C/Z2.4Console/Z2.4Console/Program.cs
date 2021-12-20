93fb0650746e30bc70276f5ef179a337c0192631
Андрей <95218345+Susa1ev@users.noreply.github.com>
2021-12-20 21:19:40 +0300

    Add files via upload

---
+++ Z02C/Z2.4Console/Z2.4Console/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Z2._4Console
+{
+    class prac
+    {
+        public prac()
+        {
+            Main();
+        }
+        void Main()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 4 - i; j >= 0; j--)
+                {
+                    Console.Write(j + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            prac p = new prac();
+        }
+    }
+}
